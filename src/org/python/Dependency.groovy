bcd5884b9d3716aebc89a4d28080776eb650da9e
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-14 03:19:11 +0530

    df

--- src/org/python/Dependency.groovy
+++ src/org/python/Dependency.groovy
@@ -2,9 +2,6 @@ package org.python
 
 def call() {
     sh '''
-      if [ ! -e /usr/bin/python ]; then
-      ln -s /usr/bin/python3 /usr/bin/python
-      fi
       python3 -m venv myenv
       . myenv/bin/activate
       snyk test --file=poetry.lock
