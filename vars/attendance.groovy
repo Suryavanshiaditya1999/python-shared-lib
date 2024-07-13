ca39aeca550e1c72d8acfdaddff7a50463d21416
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-14 00:16:59 +0530

    Update attendance.groovy

--- vars/attendance.groovy
+++ vars/attendance.groovy
@@ -2,7 +2,7 @@
 import org.python.Dependency
 // import org.python.SonarQube
 // import org.python.UnitTesting
-// import org.python.Checkout
+import org.python.Checkout
 
 def checkoutgit(String gitUrl, String branch = 'main') {
     new Checkout().call(gitUrl, branch)
