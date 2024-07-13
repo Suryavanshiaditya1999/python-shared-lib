67204c10e02a3f69d35bdc5ddf246bc74172bba8
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-14 00:01:45 +0530

    commit

---
+++ vars/attendance.groovy
@@ -0,0 +1,25 @@
+// import org.python.CoveragePython
+import org.python.Dependency
+// import org.python.SonarQube
+// import org.python.UnitTesting
+// import org.python.Checkout
+
+def checkoutgit(String gitUrl, String branch = 'main') {
+    new Checkout().call(gitUrl, branch)
+}
+
+// def callCoverage() {
+//     new Dependency.call()
+// }
+
+def callDependency() {
+    new Dependency().call()
+}
+
+// def callSonarqube() {
+//     new UnitTesting().call()
+// }
+
+// def callUnitTesting() {
+//     new UnitTesting().call()
+// }
