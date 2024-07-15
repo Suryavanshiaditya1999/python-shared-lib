af3c9539a5d1c196125343e676b6594697e7ec0e
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-16 03:01:38 +0530

    Update Dependency.groovy

--- src/org/python/Dependency.groovy
+++ src/org/python/Dependency.groovy
@@ -1,9 +1,16 @@
 package org.python
 
-def call() {
-    sh '''
-      python3 -m venv myenv
-      . myenv/bin/activate
-      snyk test --file=poetry.lock
-    '''
-}
+class DependencyCheck {
+    static void execute(script) {
+                // Run Dependency-Check using the defined tool installation
+            script.sh '''
+               python3 -m venv myenv
+               . myenv/bin/activate
+               echo "Workspace directory: ${workspaceDir}"
+               pip install -r ${workspaceDir}/requirments.txt
+               ${DEPENDENCY_CHECK_HOME}/bin/dependency-check.sh --scan . --format HTML
+            '''
+            script.archiveArtifacts artifacts: '**/dependency-check-report.html', allowEmptyArchive: true
+
+        }
+    }
