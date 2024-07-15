01e67c7a1f490ca7a45c7b8d41af5eae1e565f83
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-16 03:12:52 +0530

    Update Dependency.groovy

--- src/org/python/Dependency.groovy
+++ src/org/python/Dependency.groovy
@@ -1,16 +1,12 @@
 package org.python
 
-class DependencyCheck {
-    static void execute(script) {
-                // Run Dependency-Check using the defined tool installation
-            script.sh '''
-               python3 -m venv myenv
-               . myenv/bin/activate
-               echo "Workspace directory: ${workspaceDir}"
-               pip install -r ${workspaceDir}/requirments.txt
-               ${DEPENDENCY_CHECK_HOME}/bin/dependency-check.sh --scan . --format HTML
-            '''
-            script.archiveArtifacts artifacts: '**/dependency-check-report.html', allowEmptyArchive: true
-
-        }
-    }
+def call() {
+    sh '''
+      python3 -m venv myenv
+      . myenv/bin/activate
+      echo "Workspace directory: ${workspaceDir}"
+      pip install -r ${workspaceDir}/requirments.txt
+      ${DEPENDENCY_CHECK_HOME}/bin/dependency-check.sh --scan . --format HTML
+    '''
+    archiveArtifacts artifacts: '**/dependency-check-report.html', allowEmptyArchive: true
+}
