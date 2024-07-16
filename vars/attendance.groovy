d785943a85e39e83b3c41df5cfc9b11114d14cad
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-16 14:02:01 +0530

    Update attendance.groovy

--- vars/attendance.groovy
+++ vars/attendance.groovy
@@ -1,14 +1,14 @@
 // getting problem in code coverage
-import org.python.Checkout
+// import org.python.Checkout
 import org.python.Sonarqube
 import org.python.Dependency
 import org.python.UnitTesting
 import org.python.CodeCoverage
 
 
-def checkoutgit(String gitUrl, String branch = 'main') {
-    new Checkout().call(gitUrl, branch)
-}
+// def checkoutgit(String gitUrl, String branch = 'main') {
+//     new Checkout().call(gitUrl, branch)
+// }
 
 def sonarqubecall(String projectKey, String sourcesDir, String sonarToken) {
     new Sonarqube().call(projectKey, sourcesDir, sonarToken)
