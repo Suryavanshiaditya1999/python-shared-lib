a76121636ff8f822e7fa8260da44f28e7c38345b
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-16 13:43:07 +0530

    Create TrivyInstaller.groovy

---
+++ src/org/generic/TrivyInstaller.groovy
@@ -0,0 +1,7 @@
+package org.generic
+
+def call(String url) {   //github-token is git token, stored in jenkins as secret text
+    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+        sh "trivy repo ${url} --format json -o trivy-license-report.json"
+    }
+}
