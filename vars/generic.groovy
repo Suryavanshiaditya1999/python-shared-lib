43d6473857299af2cbef41c90b31be7703bc3908
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-16 13:43:45 +0530

    Create generic.groovy

---
+++ vars/generic.groovy
@@ -0,0 +1,16 @@
+import org.generic.Checkout
+import org.generic.Gitleaks
+import org.generic.Trivyinstaller
+
+
+def checkout(String url, String creds, String branch = 'main') {
+    new Checkout().call(url, creds, branch)
+}
+
+ def gitleaks() {
+     new Gitleaks().call()
+ }
+
+def trivyinstaller(String repoUrl) {
+     new Trivyinstaller().call(url)
+ }
