9dc744f80ab97ec8f1162796b3dae1a2c6c7572c
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-16 19:41:27 +0530

    Update Checkout.groovy

--- src/org/generic/Checkout.groovy
+++ src/org/generic/Checkout.groovy
@@ -1,10 +1,10 @@
-package org.generic
+package org.python
 
-def call(String url, String creds, String branch) {
+def call(String gitUrl, String branch = 'main') {
     stage('Checkout Git') {
         script {
-            git url: url, credentialsId: creds, branch: branch
-            echo "Checked out repository from ${url} on branch ${branch}"
+            git url: gitUrl, branch: branch
+            echo "Checked out repository from ${gitUrl} on branch ${branch}"
         }
     }
 }
